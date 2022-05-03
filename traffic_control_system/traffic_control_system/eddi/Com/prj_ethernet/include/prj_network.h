2209537b695b5d7ea0ea2c85fc4a3e24fce2633d
CodingBoxer <thsvygns123@gmail.com>
2022-05-04 01:40:55 +0900

    feat:네트워크 구조체 구현[TFICCTRL-5]

---
+++ traffic_control_system/traffic_control_system/eddi/Com/prj_ethernet/include/prj_network.h
@@ -0,0 +1,30 @@
+/*
+ * prj_network.h
+ *
+ *  Created on: 2022. 5. 3.
+ *      Author: son
+ */
+
+#ifndef EDDI_COM_PRJ_ETHERNET_INCLUDE_PRJ_NETWORK_H_
+#define EDDI_COM_PRJ_ETHERNET_INCLUDE_PRJ_NETWORK_H_
+
+#include "HL_sys_common.h"
+#include "HL_system.h"
+
+typedef struct net_info net_info;
+struct net_info{
+    ip_addr_t ip;
+    u16_t port;
+};
+
+typedef struct network network;
+struct network{
+    net_info dst;
+    net_info src;
+    uint32_t netmask;
+    uint32_t gateway;
+};
+
+
+
+#endif /* EDDI_COM_PRJ_ETHERNET_INCLUDE_PRJ_NETWORK_H_ */
