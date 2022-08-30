f81cad4514c2571b8bcf315d9a7a92ce5eacbf7f
silenc3502 <gcccompil3r@gmail.com>
2022-08-30 19:59:21 +0900

    init: Debug 경로 제거 [TFICCTRL-5]

--- traffic_control_system/TMS570LC4357_FreeRTOS_LWIP/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"/home/son/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/lib/rtsv7R4_T_be_v3D16_eabi.lib" -llibc.a
-
