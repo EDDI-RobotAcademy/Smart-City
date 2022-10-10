2a8649c829a1e95805e67e7f9136ae4d82fb1c44
silenc3502 <gcccompil3r@gmail.com>
2022-10-10 22:23:14 +0900

    공통 인터페이스 저장소 정리

--- performance_show_app/lib/core/constants/design_system.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xFF6F35A5);
-const primaryWeakColor = Color(0xFFF1E6FF);
--- performance_show_app/lib/core/storage/storage.dart
+++
@@ -1,2 +0,0 @@
-export 'local_storage.dart';
-export 'shared_preference_local_storage.dart';
