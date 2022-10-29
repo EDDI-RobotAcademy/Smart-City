a93f0b40fad77cae5d085269fd0b553c53499521
CodingBoxer <thsvygns123@gmail.com>
2022-10-29 16:32:03 +0900

    init : 추적성 관리 안되는 Legacy PR 정리를 위한 체리픽 누락사항 업로드[TRAFFIC-CONTROL]

--- performance_show_app/lib/core/storage/local_storage.dart
+++
@@ -1,6 +0,0 @@
-abstract class LocalStorage {
-  Future<bool> init();
-  void dispose();
-  void save(String key, String value);
-  String? load(String key);
-}
--- smart_city_socket_server/socket/socket_config.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SOCKET_CONFIG_H__
-#define __SOCKET_CONFIG_H__
-
-#include "common.h"
-
-void socket_config (int *sc, si *sa, int sa_size, int port);
-void err_handler (char *msg);
-
-#endif
